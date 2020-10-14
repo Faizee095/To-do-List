9cd39b57e643e06b8d044581259c4738ce66d47c
faizee <faiyazsiddique9@gmail.com>
2020-10-14 16:14:06 +0530

    EventListner

--- main.js
+++ main.js
@@ -3,3 +3,42 @@ const addTaskBtn = document.querySelector(".add-task-btn");
 const removeCompleteBtn = document.querySelector(".remove-complete-btn");
 const taskList = document.querySelector(".task-list");
 const taskTemplate = document.querySelector("#task-template");
+let id = 1;//each task is assinged a new id
+
+newTask.addEventListener("keyup", (e) => {//13 is the keycode to enter key
+  if (e.keyCode === 13 && inputValid()) {
+    addTask();
+  }
+});
+
+addTaskBtn.addEventListener("click", () => {
+  if (inputValid()) {
+    addTask();
+  }
+});
+
+removeCompleteBtn.addEventListener("click", () => {
+  const tasks = document.querySelectorAll(".task");
+  tasks.forEach((task) => {
+    const checked = task.querySelector("input").checked;
+    if (checked) {
+      task.remove();
+    }
+  });
+});
+
+function addTask() {
+  const taskElement = document.importNode(taskTemplate.content, true);
+  const checkbox = taskElement.querySelector("input");
+  checkbox.id = id;
+  const label = taskElement.querySelector("label");
+  label.htmlFor = id;
+  label.append(newTask.value);
+  taskList.appendChild(taskElement);
+  newTask.value = "";
+  id++;
+}
+
+function inputValid() {
+  return newTask.value !== "";
+}
