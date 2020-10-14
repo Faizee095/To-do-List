b3965b64c36aa61b817f5de790d778c4a2182982
faizee <faiyazsiddique9@gmail.com>
2020-10-14 14:50:55 +0530

    SCript

---
+++ main.js
@@ -0,0 +1,5 @@
+const newTask = document.querySelector("#new-task-input");
+const addTaskBtn = document.querySelector(".add-task-btn");
+const removeCompleteBtn = document.querySelector(".remove-complete-btn");
+const taskList = document.querySelector(".task-list");
+const taskTemplate = document.querySelector("#task-template");
